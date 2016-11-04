6545fb33106f2c5fdffd986dfafa1a1357a18663
Adam Copeland <adamncopeland@gmail.com>
2016-11-04 11:21:45 -0400

    Cleaned up some of the functions

--- toprelaysban.py
+++ toprelaysban.py
@@ -8,16 +8,24 @@
 import psycopg2
 import subprocess
 
-def get_pmx_top_spam_relays (threshold):
+def get_pmx_top_spam_relays (limit, threshold, interval):
 
-    # retrieves the top Spam Relays above <threshold> messages
-    #select relay, tally from prd_relays_by_type where tally >= 500 and type = 'spam'
-    # and period_start >= (now() - '1 day'::INTERVAL) order by tally desc;
+    # Retrieves the top <limit> Spam Relays
+    # Above <threshold> messages
+    # Over the last <interval>
+    # Returns the relays as a list
+    # Example: relays = get_pmx_top_spam_relays (10, 500, "1 day")
 
-    conn = psycopg2.connect("dbname=pmx_quarantine user=pmx6 host=localhost")
+    # Replace this with your database parameters. Should be the same if
+    # you are running the script locally
+
+    DB_STRING="dbname=pmx_quarantine user=pmx6 host=localhost"
+
+    conn = psycopg2.connect(DB_STRING)
     cur = conn.cursor()
+
     query = ("SELECT relay FROM prd_relays_by_type WHERE type = 'spam' AND \
-              period_start >= (now() - '1 day'::INTERVAL) ORDER BY tally DESC limit 10")
+              period_start >= (now() - \'" + str(interval) + "\'::INTERVAL) ORDER BY tally DESC limit " + str(limit))
     try:
         cur.execute(query)
         rows = cur.fetchall()
@@ -26,22 +34,22 @@ def get_pmx_top_spam_relays (threshold):
 
     return rows
 
-def read_ipblocklist ():
+def read_ipblocklist (filename):
 
     # retreives the IP blocking inclusion list and reads it into a list
     # returns: that list
 
-    with open('/opt/pmx/etc/ip-blocking-inclusions') as f:
+    with open(filename) as f:
         lines = f.read().splitlines()
 
     return lines
 
-def write_ipblocklist(blocklist):
+def write_ipblocklist(new_blocklist, filename):
 
     # writes a python list of IPs to block to the ip blocking inclusions file
 
-    ipblocklist = open('/opt/pmx/etc/ip-blocking-inclusions','w')
-    for item in blocklist:
+    ipblocklist = open(filename,'w')
+    for item in new_blocklist:
         ipblocklist.write("%s\n" % item)
 
     return
@@ -50,7 +58,7 @@ def main():
 
     # get the top spam relays that have sent more than 500 spam messages
 
-    toprelays = get_pmx_top_spam_relays(500)
+    toprelays = get_pmx_top_spam_relays(10, 500, "12 hours")
 
     # Splits the IP addresses at the last octect and converts to a subnet /24
     # This is totally arbitrary. I like to block the entire subnet block
@@ -61,10 +69,12 @@ def main():
 
     # neat trick to remove duplicates in the list. convert it to a set and back
 
-    blockedips = read_ipblocklist()
+    IP_BLOCKLIST="/opt/pmx/etc/ip-blocking-inclusions"
+
+    blockedips = read_ipblocklist(IP_BLOCKLIST)
     mergedlist = sorted(list (set(toprelays + blockedips)))
 
-    write_ipblocklist(mergedlist)
+    write_ipblocklist(mergedlist, IP_BLOCKLIST)
 
     # sync the publication up with the other nodes
     subprocess.call(['/opt/pmx6/bin/pmx-share', '--publication', 'Policy_Inbound', 'sync'], shell=False)
